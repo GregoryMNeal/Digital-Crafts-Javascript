38500a8ce897170432da8a9ead695c267046ba10
Gregory Neal <gnealsr@gmail.com>
2017-10-12 11:58:49 -0500

    Part 2 Exercise 1

--- Javascript-Exercises/Part-2-Exercise_1.js
+++ Javascript-Exercises/Part-2-Exercise_1.js
@@ -9,11 +9,11 @@ var phonebookDict = {
 }
 Write code to do the following:
 
-Print Elizabeth's phone number.
-Add a entry to the dictionary: Kareem's number is 938-489-1234.
-Delete Alice's phone entry.
-Change Bob's phone number to '968-345-2345'.
-Given this code var personName = 'Elizabeth';, use the variable personName to access the dictionary entry. Use a for...in loop to print all the phone entries.
+1. Print Elizabeth's phone number.
+2. Add a entry to the dictionary: Kareem's number is 938-489-1234.
+3. Delete Alice's phone entry.
+4. Change Bob's phone number to '968-345-2345'.
+5. Given this code var personName = 'Elizabeth';, use the variable     personName to access the dictionary entry. Use a for...in loop to print all the phone entries.
 
 */
 
@@ -21,4 +21,35 @@ var phonebookDict = {
   Alice: '703-493-1834',
   Bob: '857-384-1234',
   Elizabeth: '484-584-2923'
+}
+
+// 1. Print Elizabeth's phone number.
+
+console.log(phonebookDict['Elizabeth'], '\n');
+
+// 2. Add a entry to the dictionary: Kareem's number is 938-489-1234.
+
+phonebookDict.Kareem = '938-489-1234';
+console.log(phonebookDict);
+
+// 3. Delete Alice's phone entry.
+
+delete phonebookDict.Alice;
+console.log(phonebookDict);
+
+// 4. Change Bob's phone number to '968-345-2345'.
+
+phonebookDict.Bob = '968-345-2345';
+console.log(phonebookDict);
+
+// 5. (part 1) Given this code var personName = 'Elizabeth';, use the variable personName to access the dictionary entry.
+
+var personName = 'Elizabeth';
+console.log(phonebookDict[personName]);
+
+// 5. (part 2) Use a for...in loop to print all the phone entries. */
+
+for (var attribute in phonebookDict) {
+  var value = phonebookDict[attribute];
+  console.log(`${attribute}: ${value}`);
 }
